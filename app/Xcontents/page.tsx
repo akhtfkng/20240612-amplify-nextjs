0b18b29488989c0f93972864c92ee78e61192abf
a fukunaga <afukunaga@MacBook-Pro.local>
2024-06-16 23:09:20 +0900

    commit

--- app/Xcontents/page.tsx
+++ app/Xcontents/page.tsx
@@ -4,26 +4,20 @@ import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
 const client = new S3Client();
 
-export const S3ListObjects = async () => {
-
+export default async function Page() {
   const input = {
     Bucket: process.env.CONTENTS_BUCKET_NAME,
   };
   const command = new ListObjectsV2Command(input);
   const response = await client.send(command);
-
   console.log("Buckets: ");
   console.log(response);
-  return response;
-};
-
-export default async function Page() {
   return (
     <>
       <h1>
         xcontents
       </h1>
-      <p>{JSON.stringify(S3ListObjects)}</p>
+      <p>{JSON.stringify(response)}</p>
     </>
   );
 }
