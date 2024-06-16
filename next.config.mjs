61bc843ea95000c8ab73a2f99dc5ed8079e043ed
a fukunaga <afukunaga@MacBook-Pro.local>
2024-06-16 18:03:14 +0900

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        CONTENTS_BUCKET_NAME: process.env.CONTENTS_BUCKET_NAME,
+        CONTENTS_URL: process.env.CONTENTS_URL,
+    },
+};
 
 export default nextConfig;
