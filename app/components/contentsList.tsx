14a2bac1ecb9b3e3a44659f1e5f35fefb4bf5387
a fukunaga <afukunaga@MacBook-Pro.local>
2024-06-26 22:21:58 +0900

    commit

--- app/components/contentsList.tsx
+++ app/components/contentsList.tsx
@@ -1,37 +1,30 @@
-'use client';
+// 'use client';
 
-import React, { useState, useEffect } from 'react';
-import { listFiles } from '../lib/s3';
+// import React, { useState, useEffect } from 'react';
+import ListProps from '../lib/s3';
 
 const FileList = () => {
-  const [files, setFiles] = useState<any[]>([]);
-  const [error, setError] = useState(null);
+  // const [files, setFiles] = useState<any[]>([]);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function fetchFiles() {
-      try {
-        const files:any = await listFiles();
-        setFiles(files);
-      } catch (error:any) {
-        console.error('Error fetching files:', error);
-        setError(error.message);
-      }
-    }
+  // useEffect(() => {
+  //   async function fetchFiles() {
+  //     try {
+  //       const files:any = await listFiles();
+  //       setFiles(files);
+  //     } catch (error:any) {
+  //       console.error('Error fetching files:', error);
+  //       setError(error.message);
+  //     }
+  //   }
 
-    fetchFiles();
-  }, []);
+  //   fetchFiles();
+  // }, []);
 
   return (
     <div>
       <h1>File List</h1>
-      {error && <p>Error fetching files: {error}</p>}
-      <ul>
-        {files.map((file, index) => (
-          <li key={index}>
-            {file.path} - Last modified: {new Date(file.lastModified).toLocaleString()}
-          </li>
-        ))}
-      </ul>
+      <ListProps />
     </div>
   );
 };
--- app/lib/s3.tsx
+++ app/lib/s3.tsx
@@ -1,15 +1,14 @@
 import { list } from 'aws-amplify/storage';
 
-export async function listFiles() {
-    try {
-        const result = await list({
-            path: '/'
-        });
-        return result;
-    } catch (error) {
-        console.error('Test');
-        console.error('Error listing files:', error);
-        throw error;
-        // return [];
-    }
+const ListProps = async() => {
+    const result = await list({
+        path: 'test/'
+    });
+    return (
+      <ul>
+        {result.items.length}
+      </ul>
+    )
 }
+  
+export default ListProps;
