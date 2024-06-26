a0b3bdd84f8496052cdcab47a600bbb2c41d6f1e
a fukunaga <afukunaga@MacBook-Pro.local>
2024-06-26 22:28:33 +0900

    commit

--- app/lib/s3.tsx
+++ app/lib/s3.tsx
@@ -1,12 +1,13 @@
 import { list } from 'aws-amplify/storage';
 
 const ListProps = async() => {
-    const result = await list({
-        path: 'test/'
-    });
+    // const result = await list({
+    //     path: 'test/'
+    // });
     return (
       <ul>
-        {result.items.length}
+        {/* {result.items.length} */}
+        <p>test</p>
       </ul>
     )
 }
